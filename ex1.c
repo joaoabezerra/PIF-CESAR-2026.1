037e70222f396878cb1a002c9405fb6951e7b0a2
João Antonio Bezerra <gamestooony@outlook.com>
2026-08-24 16:07:45 -0300

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int anolet;
+    anolet = 2026;
+    printf("ano letivo: %d\n", anolet);
+    return 0;
+}
---
+++ ex4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    printf("existem %d semanas no ano letivo\n", 52);
+    printf("\n");
+    system("pause");
+    return 0;
+}
