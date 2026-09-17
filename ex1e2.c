ddd64ed6ae2131cbc713950e01776a09f5c4b08e
João Antonio B. F. de Medeiros <gamestooony@outlook.com>
2026-09-16 21:50:02 -0300

    Refactor comments and clarify integer coercion
    Updated comments and clarified type coercion in integer assignment.

--- ex1e2.c
+++ ex1e2.c
@@ -8,6 +8,10 @@ int main() {
     system("pause");
     return 0;
 }
+// (A) saída: 2
+// (B) ocorre uma coerção implícita. pois ao atribuir o valor em ponto flutuante 2.97 na variável int, o compilador descarta a parte fracionária sem realizar arredondamento.
+// (C) para evitar as perdas invertidas, utiliza-se um (int)valor para documentar a intenção, ou utilizar <math.h> como round() antes de converter para int().
+
 // (2)
 // (A) é uma biblioteca legada do MS-DOS não especificada pelo padrão ANSI C.
 // (B) getchar(), fgetc(stdin) e getc(stdin)
